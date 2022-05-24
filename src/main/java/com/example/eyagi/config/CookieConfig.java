ed21ed9f19af9d1af3ea0887430a53bfcbb9539a
EunhyeSong <dmsgp8292@gmail.com>
2022-05-24 18:59:27 +0900

    test5..

--- src/main/java/com/example/eyagi/config/CookieConfig.java
+++ src/main/java/com/example/eyagi/config/CookieConfig.java
@@ -1,30 +1,30 @@
-//package com.example.eyagi.config;
-//
-//import org.apache.tomcat.util.http.LegacyCookieProcessor;
-//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-//import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.net.MalformedURLException;
-//
-///**
-// * Solving the problem of cookie root domain name setting
-// * @author Declan
-// */
-//@Configuration
-//public class CookieConfig {
-//    /**
-//     * Solve the problem:
-//     * There was an unexpected error (type=Internal Server Error, status=500).
-//     * An invalid domain [.xxx.com] was specified for this cookie
-//     *
-//     * @return
-//     */
-//    @Bean
-//    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
-//        return (factory) -> factory.addContextCustomizers(
-//                (context) -> context.setCookieProcessor(new LegacyCookieProcessor()));
-//    }
-//
-//}
+package com.example.eyagi.config;
+
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.MalformedURLException;
+
+/**
+ * Solving the problem of cookie root domain name setting
+ * @author Declan
+ */
+@Configuration
+public class CookieConfig {
+    /**
+     * Solve the problem:
+     * There was an unexpected error (type=Internal Server Error, status=500).
+     * An invalid domain [.xxx.com] was specified for this cookie
+     *
+     * @return
+     */
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+        return (factory) -> factory.addContextCustomizers(
+                (context) -> context.setCookieProcessor(new LegacyCookieProcessor()));
+    }
+
+}
