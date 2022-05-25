b6764d11bd0374a7c019f24e05798c50c8bd4c19
EunhyeSong <dmsgp8292@gmail.com>
2022-05-25 14:17:20 +0900

    visiter test

---
+++ src/main/java/com/example/eyagi/repository/ClientAdressRepository.java
@@ -0,0 +1,7 @@
+package com.example.eyagi.repository;
+
+import com.example.eyagi.model.ClientAddress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientAdressRepository extends JpaRepository<ClientAddress, Long> {
+}
