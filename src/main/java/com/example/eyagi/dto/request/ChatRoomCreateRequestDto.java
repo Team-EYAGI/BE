2a36963eafd0a99252dbe1bd66e728e45d9531b1
EunhyeSong <dmsgp8292@gmail.com>
2022-05-31 05:17:34 +0900

    N +1 진행중

--- src/main/java/com/example/eyagi/dto/request/ChatRoomCreateRequestDto.java
+++ src/main/java/com/example/eyagi/dto/request/ChatRoomCreateRequestDto.java
@@ -1,13 +1,13 @@
-//package com.example.eyagi.dto.request;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//
-//@Getter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class ChatRoomCreateRequestDto {
-//    private String chatRoomName;
-//    private String uuid;
-//}
+package com.example.eyagi.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoomCreateRequestDto {
+    private String chatRoomName;
+    private String uuid;
+}
